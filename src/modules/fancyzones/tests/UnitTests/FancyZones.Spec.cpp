9cfdf44decc5cc45c2db2c397c11635ecc400d3b
Serafima Zykova <zykovas91@gmail.com>
2020-01-27 18:43:40 +0300

    added FancyZones unit tests

---
+++ src/modules/fancyzones/tests/UnitTests/FancyZones.Spec.cpp
@@ -0,0 +1,233 @@
+#include "pch.h"
+
+#include <iomanip>
+
+#include <lib/FancyZones.h>
+#include <lib/Settings.h>
+
+#include "util.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FancyZonesUnitTests
+{
+    TEST_CLASS(FancyZonesUnitTests)
+    {
+        HINSTANCE m_hInst;
+        winrt::com_ptr<IFancyZonesSettings> m_settings;
+
+        TEST_METHOD_INITIALIZE(Init)
+        {
+            m_hInst = (HINSTANCE)GetModuleHandleW(nullptr);
+            m_settings = MakeFancyZonesSettings(m_hInst, L"FancyZonesUnitTests");
+            Assert::IsTrue(m_settings != nullptr);
+        }
+
+        TEST_METHOD(Create)
+        {
+            auto actual = MakeFancyZones(m_hInst, m_settings);
+            Assert::IsNotNull(actual.get());
+        }
+        TEST_METHOD(CreateWithEmptyHinstance)
+        {
+            auto actual = MakeFancyZones({}, m_settings);
+            Assert::IsNotNull(actual.get());
+        }
+
+        TEST_METHOD(CreateWithNullHinstance)
+        {
+            auto actual = MakeFancyZones(nullptr, m_settings);
+            Assert::IsNotNull(actual.get());
+        }
+
+        TEST_METHOD(CreateWithNullSettings)
+        {
+            auto actual = MakeFancyZones(m_hInst, nullptr);
+            Assert::IsNotNull(actual.get());
+        }
+
+        TEST_METHOD(Run)
+        {
+            auto actual = MakeFancyZones(m_hInst, m_settings);
+
+            std::vector<std::thread> threads;
+            std::atomic<int> counter = 0;
+            const int expectedCount = 10;
+
+            auto runFunc = [&]() {
+                actual->Run();
+                counter++;
+            };
+
+            for (int i = 0; i < expectedCount; i++)
+            {
+                threads.push_back(std::thread(runFunc));
+            }
+
+            for (auto& thread : threads)
+            {
+                thread.join();
+            }
+
+            Assert::AreEqual(expectedCount, counter.load());
+        }
+
+        TEST_METHOD(Destroy)
+        {
+            auto actual = MakeFancyZones(m_hInst, m_settings);
+
+            std::vector<std::thread> threads;
+            std::atomic<int> counter = 0;
+            const int expectedCount = 10;
+
+            auto destroyFunc = [&]() {
+                actual->Destroy();
+                counter++;
+            };
+
+            for (int i = 0; i < expectedCount; i++)
+            {
+                threads.push_back(std::thread(destroyFunc));
+            }
+
+            for (auto& thread : threads)
+            {
+                thread.join();
+            }
+
+            Assert::AreEqual(expectedCount, counter.load());
+        }
+
+        TEST_METHOD(RunDestroy)
+        {
+            auto actual = MakeFancyZones(m_hInst, m_settings);
+
+            std::vector<std::thread> threads;
+            std::atomic<int> counter = 0;
+            const int expectedCount = 20;
+
+            auto func = [&]() {
+                auto idHash = std::hash<std::thread::id>()(std::this_thread::get_id());
+                bool run = (idHash % 2 == 0);
+                run ? actual->Run() : actual->Destroy();
+                counter++;
+            };
+
+            for (int i = 0; i < expectedCount; i++)
+            {
+                threads.push_back(std::thread(func));
+            }
+
+            for (auto& thread : threads)
+            {
+                thread.join();
+            }
+
+            Assert::AreEqual(expectedCount, counter.load());
+        }
+    };
+
+    TEST_CLASS(FancyZonesIZoneWindowHostUnitTests)
+    {
+        HINSTANCE m_hInst{};
+        winrt::com_ptr<IFancyZonesSettings> m_settings = nullptr;
+        winrt::com_ptr<IZoneWindowHost> m_zoneWindowHost = nullptr;
+
+        std::wstring serializedPowerToySettings(const Settings& settings)
+        {
+            PowerToysSettings::Settings ptSettings(HINSTANCE{}, L"FancyZonesUnitTests");
+
+            ptSettings.add_hotkey(L"fancyzones_editor_hotkey", IDS_SETTING_LAUNCH_EDITOR_HOTKEY_LABEL, settings.editorHotkey);
+            ptSettings.add_bool_toogle(L"fancyzones_shiftDrag", IDS_SETTING_DESCRIPTION_SHIFTDRAG, settings.shiftDrag);
+            ptSettings.add_bool_toogle(L"fancyzones_overrideSnapHotkeys", IDS_SETTING_DESCRIPTION_OVERRIDE_SNAP_HOTKEYS, settings.overrideSnapHotkeys);
+            ptSettings.add_bool_toogle(L"fancyzones_zoneSetChange_flashZones", IDS_SETTING_DESCRIPTION_ZONESETCHANGE_FLASHZONES, settings.zoneSetChange_flashZones);
+            ptSettings.add_bool_toogle(L"fancyzones_displayChange_moveWindows", IDS_SETTING_DESCRIPTION_DISPLAYCHANGE_MOVEWINDOWS, settings.displayChange_moveWindows);
+            ptSettings.add_bool_toogle(L"fancyzones_zoneSetChange_moveWindows", IDS_SETTING_DESCRIPTION_ZONESETCHANGE_MOVEWINDOWS, settings.zoneSetChange_moveWindows);
+            ptSettings.add_bool_toogle(L"fancyzones_virtualDesktopChange_moveWindows", IDS_SETTING_DESCRIPTION_VIRTUALDESKTOPCHANGE_MOVEWINDOWS, settings.virtualDesktopChange_moveWindows);
+            ptSettings.add_bool_toogle(L"fancyzones_appLastZone_moveWindows", IDS_SETTING_DESCRIPTION_APPLASTZONE_MOVEWINDOWS, settings.appLastZone_moveWindows);
+            ptSettings.add_bool_toogle(L"use_cursorpos_editor_startupscreen", IDS_SETTING_DESCRIPTION_USE_CURSORPOS_EDITOR_STARTUPSCREEN, settings.use_cursorpos_editor_startupscreen);
+            ptSettings.add_int_spinner(L"fancyzones_highlight_opacity", IDS_SETTINGS_HIGHLIGHT_OPACITY, settings.zoneHighlightOpacity, 0, 100, 1);
+            ptSettings.add_color_picker(L"fancyzones_zoneHighlightColor", IDS_SETTING_DESCRIPTION_ZONEHIGHLIGHTCOLOR, settings.zoneHightlightColor);
+            ptSettings.add_multiline_string(L"fancyzones_excluded_apps", IDS_SETTING_EXCLCUDED_APPS_DESCRIPTION, settings.excludedApps);
+
+            return ptSettings.serialize();
+        }
+
+        TEST_METHOD_INITIALIZE(Init)
+        {
+            m_hInst = (HINSTANCE)GetModuleHandleW(nullptr);
+            m_settings = MakeFancyZonesSettings(m_hInst, L"FancyZonesUnitTests");
+            Assert::IsTrue(m_settings != nullptr);
+
+            auto fancyZones = MakeFancyZones(m_hInst, m_settings);
+            Assert::IsTrue(fancyZones != nullptr);
+
+            m_zoneWindowHost = fancyZones.as<IZoneWindowHost>();
+            Assert::IsTrue(m_zoneWindowHost != nullptr);
+        }
+
+        TEST_METHOD(GetZoneHighlightColor)
+        {
+            const auto expected = RGB(171, 175, 238);
+            const Settings settings{
+                .shiftDrag = true,
+                .displayChange_moveWindows = true,
+                .virtualDesktopChange_moveWindows = true,
+                .zoneSetChange_flashZones = false,
+                .zoneSetChange_moveWindows = true,
+                .overrideSnapHotkeys = false,
+                .appLastZone_moveWindows = true,
+                .use_cursorpos_editor_startupscreen = true,
+                .zoneHightlightColor = L"#abafee",
+                .zoneHighlightOpacity = 45,
+                .editorHotkey = PowerToysSettings::HotkeyObject::from_settings(false, false, false, false, VK_OEM_3),
+                .excludedApps = L"app\r\napp2",
+                .excludedAppsArray = { L"APP", L"APP2" },
+            };
+
+            auto config = serializedPowerToySettings(settings);
+            m_settings->SetConfig(config.c_str());
+
+            const auto actual = m_zoneWindowHost->GetZoneHighlightColor();
+            Assert::AreEqual(expected, actual);
+        }
+
+        TEST_METHOD(GetZoneHighlightOpacity)
+        {
+            const auto expected = 88;
+            const Settings settings{
+                .shiftDrag = true,
+                .displayChange_moveWindows = true,
+                .virtualDesktopChange_moveWindows = true,
+                .zoneSetChange_flashZones = false,
+                .zoneSetChange_moveWindows = true,
+                .overrideSnapHotkeys = false,
+                .appLastZone_moveWindows = true,
+                .use_cursorpos_editor_startupscreen = true,
+                .zoneHightlightColor = L"#abafee",
+                .zoneHighlightOpacity = expected,
+                .editorHotkey = PowerToysSettings::HotkeyObject::from_settings(false, false, false, false, VK_OEM_3),
+                .excludedApps = L"app\r\napp2",
+                .excludedAppsArray = { L"APP", L"APP2" },
+            };
+
+            auto config = serializedPowerToySettings(settings);
+            m_settings->SetConfig(config.c_str());
+
+            const auto actual = m_zoneWindowHost->GetZoneHighlightOpacity();
+            Assert::AreEqual(expected, actual);
+        }
+        
+        TEST_METHOD(GetCurrentMonitorZoneSetEmpty)
+        {
+            const auto* actual = m_zoneWindowHost->GetCurrentMonitorZoneSet(Mocks::Monitor());
+            Assert::IsNull(actual);
+        }
+
+        TEST_METHOD(GetCurrentMonitorZoneSetNullMonitor)
+        {
+            const auto* actual = m_zoneWindowHost->GetCurrentMonitorZoneSet(nullptr);
+            Assert::IsNull(actual);
+        }
+    };
+}
