4473fbd96ded42a94bbf8133c998ca9b412b95db
Stefan S <sjfw332@gmail.com>
2020-03-23 22:47:00 +0100

    Use winrt to_string and to_hstring methods

--- src/common/json.cpp
+++ src/common/json.cpp
@@ -3,31 +3,6 @@
 
 #include <fstream>
 
-namespace
-{
-    // Convert an UTF8 string to a wide Unicode String
-    std::wstring utf8_decode(const std::string& str)
-    {
-        if (str.empty())
-            return std::wstring();
-        int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
-        std::wstring wstrTo(size_needed, 0);
-        MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
-        return wstrTo;
-    }
-
-    // Convert a wide Unicode string to an UTF8 string
-    std::string utf8_encode(const std::wstring& wstr)
-    {
-        if (wstr.empty())
-            return std::string();
-        int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
-        std::string strTo(size_needed, 0);
-        WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
-        return strTo;
-    }
-}
-
 namespace json
 {
     std::optional<JsonObject> from_file(std::wstring_view file_name)
@@ -38,7 +13,7 @@ namespace json
             using isbi = std::istreambuf_iterator<char>;
             std::string objStr{ isbi{ file }, isbi{} };
 
-            return JsonValue::Parse(utf8_decode(objStr)).GetObjectW();
+            return JsonValue::Parse(winrt::to_hstring(objStr)).GetObjectW();
         }
         catch (...)
         {
@@ -49,6 +24,6 @@ namespace json
     void to_file(std::wstring_view file_name, const JsonObject& obj)
     {
         std::wstring objStr{ obj.Stringify().c_str() };
-        std::ofstream{ file_name.data(), std::ios::binary } << utf8_encode(objStr);
+        std::ofstream{ file_name.data(), std::ios::binary } << winrt::to_string(objStr);
     }
 }
